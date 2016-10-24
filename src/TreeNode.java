836a7213db18dbcf85c226493691e0ced8f37b2a
Unknown <MOTYSHIZ@gmail.com>
2016-10-23 17:16:50 -0700

    Added method to expand nodes for A*

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,14 +1,46 @@
 /**
  * Created by Justin Ordonez on 10/22/2016.
  */
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class TreeNode {
-    TreeNode parent;
-    ArrayList children = new ArrayList();
-    int[][]state = new int[3][3];
+    HashSet explored = new HashSet();
+    private TreeNode parent;
+    private ArrayList children = new ArrayList();
+    protected int[][]state;
 
-    TreeNode(){
+    public void setParent(TreeNode parent){
+        this.parent = parent;
+    }
+
+    public void addChild(TreeNode child){
+        children.add(child);
+    }
+
+    public void setState(int[][] state){
+        this.state = state;
+    }
+
+    public String getStateString(){
+        String stateString = "";
+
+        for(int i=0; i < 3 ;i++){
+            for(int j = 0; j < 3 ;j++){
+                stateString += state[i][j];
+            }
+        }
+
+        return stateString;
+    }
+
+    public TreeNode clone(){
+        TreeNode clone = new TreeNode();
+        clone.state = this.state.clone();
+        clone.parent = this;
+        clone.explored = (HashSet)this.explored.clone();
+        clone.explored.add(this.getStateString());
+
+        return clone;
     }
 }
--- src/puzzle8.java
+++ src/puzzle8.java
@@ -5,6 +5,7 @@
 import java.util.Scanner;
 import java.util.PriorityQueue;
 import java.util.Stack;
+import java.util.Collections;
 
 public class puzzle8 {
     static TreeNode root = new TreeNode();
@@ -17,17 +18,41 @@ public class puzzle8 {
     private static void userOptions(){
         Scanner kb = new Scanner(System.in);
         System.out.println("What would you like to do?" +
-                "\n1)Input puzzle.");
+                "\n1) Randomly generated puzzle." +
+                "\n2) Input puzzle.");
         String input = kb.nextLine();
 
         switch(input){
             case "1":
+                randomPuzzle();
+                break;
+            case "2":
                 userPuzzle();
                 break;
             default:
                 System.out.println("Invalid Input.");
                 userOptions();
         }
+    }
+
+    static void randomPuzzle(){
+        Stack puzzlePieces = new Stack();
+        String puzzleString = "";
+
+        for(int i= 0; i < 9; i++){
+            puzzlePieces.add(Integer.toString(i));
+        }
+        Collections.shuffle(puzzlePieces);
+
+        for(int i= 0; i < 9; i++){
+            puzzleString += puzzlePieces.pop();
+        }
+
+        root.state = stringToState(puzzleString);
+        System.out.println("Generated Puzzle: ");
+        printState(root.state);
+        System.out.println("Cool and nice, kid.");
+        puzzleSolve();
     }
 
     static void userPuzzle(){
@@ -63,8 +88,10 @@ public class puzzle8 {
         }
 
         root.state = stringToState(input);
+        System.out.println("Generated Puzzle: ");
         printState(root.state);
         System.out.println("Nice, kid.");
+        puzzleSolve();
     }
 
     private static int[][] stringToState(String stateString){
@@ -88,4 +115,50 @@ public class puzzle8 {
         }
     }
 
+    private static void puzzleSolve(){
+        expandNode(root);
+    }
+
+    private static void expandNode(TreeNode nodeToExpand){
+        int zeroCol = 0, zeroRow = 0;
+
+        //locate the empty tile
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (nodeToExpand.state[i][j] == 0){
+                    zeroRow = i;
+                    zeroCol = j;
+                }
+            }
+        }
+
+        if(zeroCol > 0) {      //expand left movement
+            TreeNode leftExpand = nodeToExpand.clone();
+            leftExpand.state[zeroRow][zeroCol] = leftExpand.state[zeroRow][zeroCol - 1];
+            leftExpand.state[zeroRow][zeroCol - 1] = 0;
+            zeroCol--;
+            if(!nodeToExpand.explored.contains(leftExpand.getStateString()))nodeToExpand.addChild(leftExpand);
+        }
+        if(zeroCol < 2) {          //expand right movement
+            TreeNode rightExpand = nodeToExpand.clone();
+            rightExpand.state[zeroRow][zeroCol] = rightExpand.state[zeroRow][zeroCol + 1];
+            rightExpand.state[zeroRow][zeroCol + 1] = 0;
+            zeroCol++;
+            if(!nodeToExpand.explored.contains(rightExpand.getStateString()))nodeToExpand.addChild(rightExpand);
+        }
+        if(zeroRow > 0){            //expand up movement
+            TreeNode upExpand = nodeToExpand.clone();
+            upExpand.state[zeroRow][zeroCol] = upExpand.state[zeroRow-1][zeroCol];
+            upExpand.state[zeroRow - 1][zeroCol] = 0;
+            zeroRow--;
+            if(!nodeToExpand.explored.contains(upExpand.getStateString()))nodeToExpand.addChild(upExpand);
+        }
+        if(zeroRow < 2){            //expand down movement
+            TreeNode downExpand = nodeToExpand.clone();
+            downExpand.state[zeroRow][zeroCol] = downExpand.state[zeroRow+1][zeroCol];
+            downExpand.state[zeroRow + 1][zeroCol] = 0;
+            zeroRow++;
+            if(!nodeToExpand.explored.contains(downExpand.getStateString()))nodeToExpand.addChild(downExpand);
+        }
+    }
 }
