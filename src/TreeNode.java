b83aeee034bc7885aa8cc454b4cf46ae50889ab1
Unknown <MOTYSHIZ@gmail.com>
2016-10-24 23:37:06 -0700

    Completed project.

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,25 +1,35 @@
 /**
- * Created by Justin Ordonez on 10/22/2016.
+ * Created by Justin Ordonez
  */
+
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Comparator;
 
-public class TreeNode {
+public class TreeNode implements Comparator<TreeNode>{
     HashSet explored = new HashSet();
-    private TreeNode parent;
-    private ArrayList children = new ArrayList();
-    protected int[][]state;
 
-    public void setParent(TreeNode parent){
-        this.parent = parent;
+    protected ArrayList children = new ArrayList();
+    protected int[][]state;
+    protected int displacedTiles = 0;
+    protected int distanceSum = 0;
+    protected int depth = 0;
+    protected boolean isGoal = false;
+    protected boolean isRoot = false;
+    protected static int searchCost = 0;
+    protected TreeNode parent;
+
+    TreeNode(){
+        searchCost++;
+    }
+
+    TreeNode(boolean isRoot){
+        this.isRoot = isRoot;
+        searchCost++;
     }
 
     public void addChild(TreeNode child){
         children.add(child);
-    }
-
-    public void setState(int[][] state){
-        this.state = state;
     }
 
     public String getStateString(){
@@ -36,11 +46,60 @@ public class TreeNode {
 
     public TreeNode clone(){
         TreeNode clone = new TreeNode();
-        clone.state = this.state.clone();
+        clone.state = arrayCopy(this.state);
         clone.parent = this;
-        clone.explored = (HashSet)this.explored.clone();
+        clone.explored = new HashSet(this.explored);
         clone.explored.add(this.getStateString());
-
+        clone.depth = clone.explored.size();
         return clone;
+    }
+
+    public int heuristicOne(){
+        displacedTiles = 0;
+
+        for(int i=0; i < 3 ;i++){
+            for(int j = 0; j < 3 ;j++){
+                if(state[i][j] != ((i*3)+j))displacedTiles++;
+            }
+        }
+
+        if(displacedTiles == 0)isGoal = true;
+        return displacedTiles + depth;
+    }
+
+    public int heuristicTwo(){
+        distanceSum = 0;
+
+        for(int i = 0; i < state.length ; i++){
+            for(int j = 0; j < state[i].length; j++){
+                if(state[i][j] != 0){
+                    distanceSum += Math.abs((state[i][j] / 3) - i) + Math.abs((state[i][j] % 3) - j);
+                }
+            }
+        }
+
+        if(distanceSum == 0)isGoal = true;
+        return distanceSum + depth;
+    }
+
+    public int[][] arrayCopy(int[][] array){
+        int[][] copy = new int[3][3];
+
+        for(int i=0; i < 3 ;i++){
+            for(int j = 0; j < 3 ;j++){
+                copy[i][j] = array[i][j];
+            }
+        }
+
+        return copy;
+    }
+
+    @Override
+    public int compare(TreeNode o1, TreeNode o2) {
+        if(puzzle8.useHeuristicOne){
+            return o1.heuristicOne() - o2.heuristicOne();
+        }else{
+            return o1.heuristicTwo() - o2.heuristicTwo();
+        }
     }
 }
--- src/puzzle8.java
+++ src/puzzle8.java
@@ -1,37 +1,76 @@
 /**
- * Created by pc on 10/20/2016.
+ * Created by Justin Ordonez
  */
 
-import java.util.Scanner;
-import java.util.PriorityQueue;
-import java.util.Stack;
-import java.util.Collections;
+import sun.reflect.generics.tree.Tree;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
 
 public class puzzle8 {
-    static TreeNode root = new TreeNode();
-    PriorityQueue frontier = new PriorityQueue();
+    static TreeNode current = new TreeNode(true);
+    static Comparator<TreeNode> nodeComparator = new TreeNode();
+    static PriorityQueue<TreeNode> frontier = new PriorityQueue(10, nodeComparator);
+    static boolean useHeuristicOne = true;
+    static boolean statisticMode = false;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
         userOptions();
     }
 
-    private static void userOptions(){
+    private static void userOptions() throws FileNotFoundException {
         Scanner kb = new Scanner(System.in);
         System.out.println("What would you like to do?" +
                 "\n1) Randomly generated puzzle." +
-                "\n2) Input puzzle.");
+                "\n2) Input puzzle." +
+                "\n3) Randomly generate 101 puzzles." +
+                "\n4) Read 200 predefined test cases.");
         String input = kb.nextLine();
 
         switch(input){
             case "1":
+                heuristicChoose();
                 randomPuzzle();
                 break;
             case "2":
                 userPuzzle();
                 break;
+            case "3":
+                statisticMode = true;
+                heuristicChoose();
+                oneHundredAndOnePuzzles();
+                break;
+            case "4":
+                statisticMode = true;
+                heuristicChoose();
+                twoHundredReadInPuzzles();
+                break;
             default:
                 System.out.println("Invalid Input.");
                 userOptions();
+        }
+    }
+
+    private static void heuristicChoose(){
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Which heuristic would you lke to use?" +
+                "\n1) The number of misplaced tiles." +
+                "\n2) The sum of distances of the tiles from their goal positions.");
+        String input = kb.nextLine();
+
+        switch(input){
+            case "1":
+                System.out.println("Using heuristic one.");
+                break;
+            case "2":
+                System.out.println("Using heuristic two.");
+                useHeuristicOne = false;
+                break;
+            default:
+                System.out.println("Invalid Input.");
+                heuristicChoose();
         }
     }
 
@@ -48,11 +87,19 @@ public class puzzle8 {
             puzzleString += puzzlePieces.pop();
         }
 
-        root.state = stringToState(puzzleString);
-        System.out.println("Generated Puzzle: ");
-        printState(root.state);
-        System.out.println("Cool and nice, kid.");
-        puzzleSolve();
+        current.state = stringToState(puzzleString);
+
+
+        if(!solvableCheck(current)){
+            randomPuzzle();
+        }else if(current.getStateString().contains("012345678")){
+            System.out.println("I somehow managed to randomly generate an already-complete puzzle. C R A Z Y ! ! !");
+            randomPuzzle();
+        }else{
+            System.out.println("\nGenerated Puzzle: ");
+            printState(current);
+            puzzleSolve();
+        }
     }
 
     static void userPuzzle(){
@@ -76,6 +123,9 @@ public class puzzle8 {
             if(input.length() != 9){
                 System.out.println("String is not 9 characters long.");
                 stringAccepted = false;
+            }else if(input.contains("012345678")){
+                System.out.println("That is an already completed puzzle!");
+                stringAccepted = false;
             }else{
                 for(int i = 0; i < 9; i++){
                     String checkCharacter = stringChecklist.pop().toString();
@@ -87,10 +137,11 @@ public class puzzle8 {
             }
         }
 
-        root.state = stringToState(input);
-        System.out.println("Generated Puzzle: ");
-        printState(root.state);
-        System.out.println("Nice, kid.");
+        heuristicChoose();
+        current.state = stringToState(input);
+        System.out.println("\nGenerated Puzzle: ");
+        printState(current);
+        System.out.println("Solving puzzle: ");
         puzzleSolve();
     }
 
@@ -106,17 +157,44 @@ public class puzzle8 {
         return state;
     }
 
-    private static void printState(int[][] state){
+    private static void printState(TreeNode node){
         for(int i=0; i < 3 ;i++){
             for(int j = 0; j < 3 ;j++){
-                System.out.print(state[i][j] + " ");
+                System.out.print(node.state[i][j] + " ");
             }
             System.out.println();
         }
     }
 
     private static void puzzleSolve(){
-        expandNode(root);
+        Stack<TreeNode> pathStack = new Stack();
+        int solutionDepth;
+        int numOfMoves = 0;
+
+        do {
+            expandNode(current);
+            addChildrenToFrontier(current);
+            current = frontier.poll();
+        } while(!current.isGoal && frontier.size() > 0);
+
+        solutionDepth = current.depth;
+
+        if(!statisticMode){
+            while(!current.isRoot){
+                pathStack.add(current);
+                current = current.parent;
+            }
+            pathStack.add(current);
+
+            while(pathStack.size() > 0){
+                numOfMoves++;
+                System.out.println("Move #" + numOfMoves + ":");
+                printState(pathStack.pop());
+            }
+        }
+
+        System.out.println("Search Cost: " + current.searchCost);
+        System.out.println("Solution Depth: " + solutionDepth);
     }
 
     private static void expandNode(TreeNode nodeToExpand){
@@ -136,29 +214,125 @@ public class puzzle8 {
             TreeNode leftExpand = nodeToExpand.clone();
             leftExpand.state[zeroRow][zeroCol] = leftExpand.state[zeroRow][zeroCol - 1];
             leftExpand.state[zeroRow][zeroCol - 1] = 0;
-            zeroCol--;
             if(!nodeToExpand.explored.contains(leftExpand.getStateString()))nodeToExpand.addChild(leftExpand);
         }
         if(zeroCol < 2) {          //expand right movement
             TreeNode rightExpand = nodeToExpand.clone();
             rightExpand.state[zeroRow][zeroCol] = rightExpand.state[zeroRow][zeroCol + 1];
             rightExpand.state[zeroRow][zeroCol + 1] = 0;
-            zeroCol++;
             if(!nodeToExpand.explored.contains(rightExpand.getStateString()))nodeToExpand.addChild(rightExpand);
         }
         if(zeroRow > 0){            //expand up movement
             TreeNode upExpand = nodeToExpand.clone();
             upExpand.state[zeroRow][zeroCol] = upExpand.state[zeroRow-1][zeroCol];
             upExpand.state[zeroRow - 1][zeroCol] = 0;
-            zeroRow--;
             if(!nodeToExpand.explored.contains(upExpand.getStateString()))nodeToExpand.addChild(upExpand);
         }
         if(zeroRow < 2){            //expand down movement
             TreeNode downExpand = nodeToExpand.clone();
             downExpand.state[zeroRow][zeroCol] = downExpand.state[zeroRow+1][zeroCol];
             downExpand.state[zeroRow + 1][zeroCol] = 0;
-            zeroRow++;
             if(!nodeToExpand.explored.contains(downExpand.getStateString()))nodeToExpand.addChild(downExpand);
         }
+    }
+
+    private static void addChildrenToFrontier(TreeNode parentNode){
+        for(Object x: parentNode.children){
+            frontier.add((TreeNode)x);
+        }
+    }
+
+    private static boolean solvableCheck(TreeNode node){
+        int invertedTiles = 0;
+        String stateString = node.getStateString();
+
+        for(int i = 0; i < stateString.length(); i++){
+            for(int j = i+1; j < stateString.length(); j++){
+                if(Character.getNumericValue(stateString.charAt(j)) <
+                        Character.getNumericValue(stateString.charAt(i))
+                        && Character.getNumericValue(stateString.charAt(j)) != 0) {
+                    invertedTiles++;
+                }
+            }
+        }
+
+        if(invertedTiles % 2 == 0) return true;
+        else return false;
+    }
+
+    private static void oneHundredAndOnePuzzles(){
+        int avgSearchCost = 0;
+        long startTime, endTime;
+        long avgExecTime = 0;
+
+        for(int i=0 ; i <= 101; i++){
+            startTime = System.currentTimeMillis();
+            randomPuzzle();
+            endTime = System.currentTimeMillis();
+            avgExecTime += endTime - startTime;
+            avgSearchCost += TreeNode.searchCost;
+            TreeNode.searchCost = 0;
+            frontier.clear();
+            current = new TreeNode(true);
+        }
+
+        avgSearchCost = avgSearchCost/101;
+        avgExecTime = avgExecTime/101;
+        System.out.println("Average Search Cost for 101 cases: " + avgSearchCost
+                    + "\nAverage Execution Time for 101 cases: " + avgExecTime + "ms");
+    }
+
+    private static void twoHundredReadInPuzzles() throws FileNotFoundException{
+        Scanner in = new Scanner(new FileReader("200 Scrambled Puzzles.txt"));
+        int depthLevel = 0;
+        int amtInDepthLvl = 0;
+        int avgSearchCost = 0;
+        long avgExecTime = 0;
+        long startTime, endTime;
+        String currentLine;
+
+        while(in.hasNext()){
+            currentLine = in.next();
+            if(currentLine.contains("Depth")){
+                if(depthLevel != 0){
+                    avgSearchCost = avgSearchCost/amtInDepthLvl;
+                    avgExecTime = avgExecTime/amtInDepthLvl;
+                    System.out.println("Average Search Cost for " + amtInDepthLvl + " cases in Depth " + depthLevel + ": "
+                            + avgSearchCost
+                            + "\nAverage Execution Time: " + avgExecTime + "ms");
+                }
+
+                depthLevel = in.nextInt();
+                System.out.println("\nDepth: " + depthLevel);
+
+                amtInDepthLvl = 0;
+                avgSearchCost = 0;
+                avgExecTime = 0;
+            } else {
+                startTime = System.currentTimeMillis();
+                puzzleReader(currentLine);
+                endTime = System.currentTimeMillis();
+                avgExecTime += endTime - startTime;
+                amtInDepthLvl++;
+            }
+
+            avgSearchCost += TreeNode.searchCost;
+            TreeNode.searchCost = 0;
+            frontier.clear();
+            current = new TreeNode(true);
+        }
+
+        avgSearchCost = avgSearchCost/amtInDepthLvl;
+        avgExecTime = avgExecTime/amtInDepthLvl;
+        System.out.println("Average Search Cost for " + amtInDepthLvl + " cases in Depth " + depthLevel + ": "
+                + avgSearchCost
+                + "\nAverage Execution Time: " + avgExecTime + "ms");
+    }
+
+    private static void puzzleReader(String input){
+        current.state = stringToState(input);
+        System.out.println("\nGenerated Puzzle: ");
+        printState(current);
+        puzzleSolve();
     }
 }
